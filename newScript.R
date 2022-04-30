72ad080e3166d394802d585e33853704edb4a7c7
michael-bariotakis <navaak@gmail.com>
2022-04-30 15:30:29 +0200

    Commit new script

---
+++ newScript.R
@@ -0,0 +1,8 @@
+#' This comment should show up in the text.
+#' The same applies to this text
+ 
+x <- seq(-3, 3, by = .01)
+y <- pnorm(x)
+plot(x, y, type = 'l', col = 'firebrick')
+
+#' And finally, this comment should be visible as well.
