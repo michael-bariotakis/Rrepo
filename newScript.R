7c38102b6c74702611746dbdbc68f6114418695e
michael-bariotakis <navaak@gmail.com>
2022-04-30 15:34:04 +0200

    Commit add YAML header in newScript

--- newScript.R
+++ newScript.R
@@ -1,5 +1,10 @@
-#' This comment should show up in the text.
-#' The same applies to this text
+#' ---
+#' title: "This new Script was not initially created with Markdown"
+#' output: github_document
+#' ---
+#' This comment should show up in the text. And it is still, 
+#' just a comment. Nothing fancy going on.
+#' The same applies to this text over here.
  
 x <- seq(-3, 3, by = .01)
 y <- pnorm(x)
